59e1faa034e1f0286e27b3e8b4f57f672874b740
Henk Valk <info@henkvalk.nl>
2024-03-04 14:32:27 +0100

    IMP: lazyload javascript && update Copyrights information

--- view/frontend/tailwind/tailwind.config.js
+++ view/frontend/tailwind/tailwind.config.js
@@ -1,3 +1,12 @@
+/**
+ * @author      Henk Valk (henk@falconmedia.nl)
+ * @author      Ruud van Zuidam (allrude@gmail.com)
+ * @package     Belco_Hyva
+ * @since       version 0.4
+ * Copyrights  2024 Falcon Media. All rights reserved.
+ * https://www.falconmedia.nl
+ */
+
 module.exports = {
     purge: {
         content: ["../templates/**/*.{xml,phtml}"],
